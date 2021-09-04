afee023f2e32507c2428fbe57fc28589c59b081d
rifqiAly <rifqi.aly@gmail.com>
2021-09-04 09:19:11 +0700

    register form created, will finish animation by this evening

--- src/App.js
+++ src/App.js
@@ -2,33 +2,18 @@
 import './App.css';
 import React from 'react'
 
-// const inputs = document.querySelectorAll('input')
-
-// function focusFunc(){
-//   let parent = this.parentNode.parentNode;
-//   parent.classList.add('focus')
-// }
-
-// function blurFunc(){
-//   let parent = this.parentNode.parentNode;
-//   if(this.value===""){
-//     parent.classList.remove('focus')
-//   }
-// }
-
-// inputs.forEach(input =>{
-//   input.addEventListener('focus', focusFunc)
-//   input.addEventListener('blur', blurFunc)
-// })
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      classUsername : 'input-div one',
-      classPassword : 'input-div two',
-      username: '',
-      password: ''
+      classUsername : 'input-div user',
+      classPassword : 'input-div password',
+      classEmail : 'input-div email',
+      loginUsername: '',
+      loginPassword: '',
+      regisEmail: '',
+      regisUsername: '',
+      regisPassword: '',
     };
   }
   render() {
@@ -40,23 +25,26 @@ class App extends React.Component {
             <img src="/svgLib/undraw_unlock_24mb.svg" alt=""/>
           </div>
           <div class="login-container">
-            <form action="">
+            <form action="sign-in">
               <img class="lap" src="/svgLib/undraw_secure_login_pdn4.svg" alt=""/>
               <h2>site name</h2>
+              <h5 class="quote">Not registered yet?</h5>
+              <a class="toggle">Sign Up</a>
+              
               <div class={this.state.classUsername}
                 onFocus={() =>{
                   this.setState({classUsername: 'input-div one focus'});
                 }}
                 onBlur={() =>{
-                  if (this.state.username) {
+                  if (this.state.loginUsername) {
                     this.setState({classUsername: 'input-div one focus'});
-                  }else if(this.state.user===''){
+                  }else if(this.state.loginUsername===''){
                     this.setState({classUsername: 'input-div one'});
                   }else{
                     this.setState({classUsername: 'input-div one'});
                   }
                 }}
-                value={this.state.username} onChange={(event) => this.setState({username: event.target.value})}
+                value={this.state.loginUsername} onChange={(event) => this.setState({loginUsername: event.target.value})}
               >
                 <div class="i">
                   <i class="fas fa-user"></i>
@@ -71,7 +59,7 @@ class App extends React.Component {
                   this.setState({classPassword: 'input-div two focus'});
                 }}
                 onBlur={() =>{
-                  if (this.state.password) {
+                  if (this.state.loginPassword) {
                     this.setState({classPassword: 'input-div two focus'});
                   }else if(this.state.user===''){
                     this.setState({classPassword: 'input-div two'});
@@ -79,7 +67,7 @@ class App extends React.Component {
                     this.setState({classPassword: 'input-div two'});
                   }
                 }}
-                value={this.state.password} onChange={(event) => this.setState({password: event.target.value})}
+                value={this.state.loginPassword} onChange={(event) => this.setState({loginPassword: event.target.value})}
               >
                 <div class="i">
                   <i class="fas fa-lock"></i>
@@ -91,6 +79,88 @@ class App extends React.Component {
               </div>
               <a>Forgot Password?</a>
               <input type="submit" class="btn" value="Login"/>
+            </form>
+          </div>
+          <div class="register-container">
+            <form action="sign-up">
+              <img class="lap" src="/svgLib/undraw_security_o890.svg" alt=""/>
+              <h2>site name</h2>
+              <h5 class="quote">Already have an account?</h5>
+              <a class="toggle">Sign In</a>
+              
+              <div class={this.state.classUsername}
+                onFocus={() =>{
+                  this.setState({classUsername: 'input-div one focus'});
+                }}
+                onBlur={() =>{
+                  if (this.state.regisUsername) {
+                    this.setState({classUsername: 'input-div one focus'});
+                  }else if(this.state.regisUsername===''){
+                    this.setState({classUsername: 'input-div one'});
+                  }else{
+                    this.setState({classUsername: 'input-div one'});
+                  }
+                }}
+                value={this.state.regisUsername} onChange={(event) => this.setState({regisUsername: event.target.value})}
+              >
+                <div class="i">
+                  <i class="fas fa-user"></i>
+                </div>
+                <div>
+                  <h5>Username</h5>
+                  <input class="input" type="text"/>
+                </div>
+              </div>
+              
+              <div class={this.state.classEmail}
+                onFocus={() =>{
+                  this.setState({classEmail: 'input-div three focus'});
+                }}
+                onBlur={() =>{
+                  if (this.state.regisEmail) {
+                    this.setState({classEmail: 'input-div three focus'});
+                  }else if(this.state.regisEmail===''){
+                    this.setState({classEmail: 'input-div three'});
+                  }else{
+                    this.setState({classEmail: 'input-div three'});
+                  }
+                }}
+                value={this.state.regisEmail} onChange={(event) => this.setState({regisEmail: event.target.value})}
+              >
+                <div class="i">
+                <i class="fas fa-envelope"></i>
+                </div>
+                <div>
+                  <h5>Email</h5>
+                  <input class="input" type="password"/>
+                </div>
+              </div>
+
+              <div class={this.state.classPassword}
+                onFocus={() =>{
+                  this.setState({classPassword: 'input-div two focus'});
+                }}
+                onBlur={() =>{
+                  if (this.state.regisPassword) {
+                    this.setState({classPassword: 'input-div two focus'});
+                  }else if(this.state.regisPassword===''){
+                    this.setState({classPassword: 'input-div two'});
+                  }else{
+                    this.setState({classPassword: 'input-div two'});
+                  }
+                }}
+                value={this.state.regisPassword} onChange={(event) => this.setState({regisPassword: event.target.value})}
+              >
+                <div class="i">
+                  <i class="fas fa-lock"></i>
+                </div>
+                <div>
+                  <h5>Password</h5>
+                  <input class="input" type="password"/>
+                </div>
+              </div>
+              
+              <input type="submit" class="btn" id="register" value="Register"/>
             </form>
           </div>
         </div>
